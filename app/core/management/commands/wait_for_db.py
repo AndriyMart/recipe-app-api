8624bd5985e132823a088c7be5eeead6df8fbabe
Andriy <martynenkoa17@gmail.com>
2024-11-28 00:00:51 +0100

    Docker config updates + git actions config update

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,11 +1,24 @@
 """
 Django command to wait for db to be available
 """
-from django.core.management import BaseCommand
+import time
 
+from django.core.management import BaseCommand
+from django.db import OperationalError
+from psycopg2 import OperationalError as Psycop2Error
 
 class Command(BaseCommand):
     """Django Command to wait for db"""
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write("Waiting for db...")
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycop2Error, OperationalError):
+                self.stdout.write(self.style.ERROR("DB unavailable, waiting 1 second..."))
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("DB available!"))
